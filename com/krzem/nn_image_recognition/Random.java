2006817d8c23d6efb86e0449a2ccba30eba32ea8
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 16:27:22 +0100

    Push Update

---
+++ com/krzem/nn_image_recognition/Random.java
@@ -0,0 +1,17 @@
+package com.krzem.nn_image_recognition;
+
+
+
+
+
+
+public class Random{
+	public static final int SEED=12345;
+	private static java.util.Random RG=new java.util.Random(SEED);
+
+
+
+	public static double next(){
+		return Random.RG.nextGaussian();
+	}
+}
