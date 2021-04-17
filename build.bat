594d2d1db1a582bed2eef270aaf861fc457ec013
Krzem <krzem5.dev@gmail.com>
2021-04-17 18:08:55 +0200

    Push Update 04/17/2021, 18:08:48

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
-echo off
-echo NUL>_.class&&del /s /f /q *.class
+@echo off
 cls
-javac -encoding utf8 com/krzem/nn_image_recognition/Main.java&&java -Dfile.encoding=UTF8 com/krzem/nn_image_recognition/Main
-start /min cmd /c "echo NUL>_.class&&del /s /f /q *.class"
+python build.py %*
